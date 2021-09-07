c83e53d8388980f5e7424d25ec26b1d8754b8b90
bwritam <bwritam@gmail.com>
2021-09-07 17:10:25 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// https://www.w3schools.com/howto/howto_js_validation_empty_input.asp
+// https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
+// https://www.w3schools.com/howto/howto_css_switch.asp
+
+
